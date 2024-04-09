15ef946cb33bfd8bc77fa57161326c31ac07d616
a01771226 <A01771226@tec.mx>
2024-04-08 19:41:13 -0600

    9 filtros

--- extraccion.py
+++ extraccion.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 
 print("Aplicación de extracción de datos")
 df = pd.read_excel("detalle_precios_productos_fabricados_2022.xlsx")
@@ -16,3 +16,31 @@ filtro2.to_csv("Entregable2.csv")
 filtro3 = df.iloc[:, [1,3,4,10]]
 print(filtro3)
 filtro3.to_csv("Entregable3.csv")
+
+df1= pd.read_excel("detalle_precios_productos_fabricados_2022.xlsx", index_col=1)
+df1
+
+filtro4=df1.loc[["2022-11-22","'2022-12-23"], ["SUBTOTAL_PARTIDA"]]
+filtro4
+
+filtro5=df.head(8)
+filtro5
+
+filtro6=df[df["SUBTOTAL_PARTIDA"] > 77000]
+filtro6
+
+filtro7=df[(df["SUBTOTAL_PARTIDA"] > 77000) & (df["FECHA_DOC"] == "2022-05-24")]
+filtro7
+
+filtro8=df[(df["SUBTOTAL_PARTIDA"] > 77000)| (df["FECHA_DOC"] == "2022-05-24")]
+filtro8
+
+filtro9=df[~(df["SUBTOTAL_PARTIDA"] > 77000) & ~(df["FECHA_DOC"] == "2022-05-24")]
+filtro9
+
+filtro4.to_csv("Entregable4.csv")
+filtro5.to_csv("Entregable5.csv")
+filtro6.to_csv("Entregable6.csv")
+filtro7.to_csv("Entregable7.csv")
+filtro8.to_csv("Entregable8.csv")
+filtro9.to_csv("Entregable9.csv")
